530eea01a5028af77bd588a24ae2b4a062339f00
EmperorAdrien <adrien.thecy@edu.ece.fr>
2023-10-11 18:08:33 +0200

    initial push

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/HomePage.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('home/home.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
