4f9d10ef7e7f8fb666a94e68f83574209deaa577
EmperorAdrien <adrien.thecy@edu.ece.fr>
2023-11-20 17:08:47 +0100

    update-templates

--- app/var/cache/dev/twig/f5/f57a2e509f14dbee756c922f23b2d9d4.php
+++ app/var/cache/dev/twig/f5/f57a2e509f14dbee756c922f23b2d9d4.php
@@ -24,10 +24,16 @@ class __TwigTemplate_adbcf57cdc47b38161e4fe51a7009e2a extends Template
 
         $this->source = $this->getSourceContext();
 
-        $this->parent = false;
-
         $this->blocks = [
+            'style' => [$this, 'block_style'],
+            'body' => [$this, 'block_body'],
         ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 2
+        return "base.html.twig";
     }
 
     protected function doDisplay(array $context, array $blocks = [])
@@ -39,91 +45,8 @@ class __TwigTemplate_adbcf57cdc47b38161e4fe51a7009e2a extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "dailystreak/dailystreak.html.twig"));
 
-        // line 2
-        echo "<head>
-    <meta charset=\"UTF-8\">
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <title>TUSMO</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #ff3399;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        .container {
-            width: 400px;
-            background-color: #006699;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        .header {
-            background-color: #E74D50;
-            padding: 15px;
-            border-radius: 5px;
-            margin-bottom: 40px;
-            font-size : 50px;
-        }
-
-        .homebutton {
-            background-color: #FFFFFF;
-            padding: 10px 20px;
-            border-radius: 5px;
-            margin-bottom: 20px;
-            border: none;
-            font-size: 1rem;
-            cursor: pointer;
-            width: 100%;
-            box-sizing: border-box;
-            filter: drop-shadow(0px 8px);
-            font-size : 30px;
-        }
-
-        .homebutton:hover {
-            background-color : #009999;
-            -webkit-transition: all 0.2s ease-in-out;
-        }
-
-        .icons {
-            display: flex;
-            justify-content: space-around;
-            margin-top: 20px;
-            width: 80%;
-            filter: drop-shadow(0px 8px);
-        }
-
-        .icon {
-            width: 24px;
-            height: 24px;
-        }
-
-    </style>
-
-
-</head>
-
-<body>
-    <div class=\"container\">
-        <div class=\"header\">TUSMO</div>
-        <button class=\"homebutton\">DAILY WORD</button>
-        <button class=\"homebutton\">DAILY STREAK</button>
-        <button class=\"homebutton\">SOLO</button>
-        <button class=\"homebutton\">MULTIPLAYER</button>
-        <div class=\"icons\">
-            <img src=\"path_to_settings_icon.png\" alt=\"Settings\" class=\"icon\">
-            <img src=\"path_to_sound_icon.png\" alt=\"Sound\" class=\"icon\">
-            <img src=\"path_to_video_icon.png\" alt=\"Video\" class=\"icon\">
-            <img src=\"path_to_flag_icon.png\" alt=\"Flag\" class=\"icon\">
-        </div>
-    </div>
-</body>
-</html>
-";
+        $this->parent = $this->loadTemplate("base.html.twig", "dailystreak/dailystreak.html.twig", 2);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
         
         $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
 
@@ -132,102 +55,90 @@ class __TwigTemplate_adbcf57cdc47b38161e4fe51a7009e2a extends Template
 
     }
 
+    // line 4
+    public function block_style($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "style"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "style"));
+
+        // line 5
+        echo "<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>Daily Word</title>
+</head>
+";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 12
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 13
+        echo "<body>
+    <div class=\"container\">
+        
+    </div>
+</body>
+";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
     public function getTemplateName()
     {
         return "dailystreak/dailystreak.html.twig";
     }
 
+    public function isTraitable()
+    {
+        return false;
+    }
+
     public function getDebugInfo()
     {
-        return array (  43 => 2,);
+        return array (  94 => 13,  84 => 12,  69 => 5,  59 => 4,  36 => 2,);
     }
 
     public function getSourceContext()
     {
         return new Source("{# templates/dailystreak/dailystreak.html.twig #}
+{% extends 'base.html.twig' %}
+
+{% block style %}
 <head>
     <meta charset=\"UTF-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <title>TUSMO</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #ff3399;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        .container {
-            width: 400px;
-            background-color: #006699;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        .header {
-            background-color: #E74D50;
-            padding: 15px;
-            border-radius: 5px;
-            margin-bottom: 40px;
-            font-size : 50px;
-        }
-
-        .homebutton {
-            background-color: #FFFFFF;
-            padding: 10px 20px;
-            border-radius: 5px;
-            margin-bottom: 20px;
-            border: none;
-            font-size: 1rem;
-            cursor: pointer;
-            width: 100%;
-            box-sizing: border-box;
-            filter: drop-shadow(0px 8px);
-            font-size : 30px;
-        }
-
-        .homebutton:hover {
-            background-color : #009999;
-            -webkit-transition: all 0.2s ease-in-out;
-        }
-
-        .icons {
-            display: flex;
-            justify-content: space-around;
-            margin-top: 20px;
-            width: 80%;
-            filter: drop-shadow(0px 8px);
-        }
-
-        .icon {
-            width: 24px;
-            height: 24px;
-        }
-
-    </style>
-
-
+    <title>Daily Word</title>
 </head>
+{% endblock %}
 
+{% block body %}
 <body>
     <div class=\"container\">
-        <div class=\"header\">TUSMO</div>
-        <button class=\"homebutton\">DAILY WORD</button>
-        <button class=\"homebutton\">DAILY STREAK</button>
-        <button class=\"homebutton\">SOLO</button>
-        <button class=\"homebutton\">MULTIPLAYER</button>
-        <div class=\"icons\">
-            <img src=\"path_to_settings_icon.png\" alt=\"Settings\" class=\"icon\">
-            <img src=\"path_to_sound_icon.png\" alt=\"Sound\" class=\"icon\">
-            <img src=\"path_to_video_icon.png\" alt=\"Video\" class=\"icon\">
-            <img src=\"path_to_flag_icon.png\" alt=\"Flag\" class=\"icon\">
-        </div>
+        
     </div>
 </body>
-</html>
-", "dailystreak/dailystreak.html.twig", "/var/www/templates/dailystreak/dailystreak.html.twig");
+{% endblock %}", "dailystreak/dailystreak.html.twig", "/var/www/templates/dailystreak/dailystreak.html.twig");
     }
 }
